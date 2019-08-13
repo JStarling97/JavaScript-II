255fed923dd038a66fab84c2b46d7e06052a6cfc
Joshua Starling <joshstarling97@gmail.com>
2019-08-13 18:37:06 -0400

    working on array methods

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum',];
 
 /* 
 
@@ -27,19 +27,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(items.length);
+  console.log(getLength);
 }
+//   console.log(`The Length is: ${items.length}`);
+// }
+// getLength();
 
+const cb = x => {
+  console.log("Last item is: ", x);
+  return x;
+}
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[items.length - 1]);
 }
+last(items, cb);
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+sumNums(7, 7, answer => {
+  console.log(`The Answer is: ${answer}.`)
+})
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+
+multiplyNums(7, 7, answer => {
+  console.log(`The answer is: ${answer}.`)
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
